8054c6554d00d12c88fd8e26fda74e404517d1fd
Davvidar <daargente@gmail.com>
2024-02-22 10:42:56 +0100

    mvc

--- database/config.ts
+++ database/config.ts
@@ -5,6 +5,6 @@ const CONFIG = {
     port: 3306,
     user: 'root',
     password: '1234',
-    database: 'ecommerceBackend'
+    database: 'db_marketplace'
 };
 export const connection = createPool(CONFIG);
